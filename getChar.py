ab86f020d2cbe24576295fefb72827963cda3d65
Tran Trung Hieu <tthieuhcm@gmail.com>
2018-11-25 21:07:04 +0700

    Fix threshold and conditions

--- getChar.py
+++ getChar.py
@@ -17,8 +17,11 @@ def isNearlyMaxLength(contour, image):
     # type: (ndarray, ndarray) -> int
     even = contour.flatten()[0::2]  # X-axis
     odd = contour.flatten()[1::2]  # Y-axis
-    if even.max() - even.min() > 0.4 * image.shape[0] or \
-            odd.max() - odd.min() > 0.6 * image.shape[1]:
+    adaptThreshold = 0.57
+    if 2 * image.shape[0] < image.shape[1]:
+        adaptThreshold = 0.7
+    if even.max() - even.min() > 0.4 * image.shape[1] or \
+            odd.max() - odd.min() > adaptThreshold * image.shape[0]:
         return True
     else:
         return False
@@ -28,7 +31,26 @@ def isNearlyMinLength(contour, image):
     # type: (ndarray, ndarray) -> bool
     even = contour.flatten()[0::2]  # X-axis
     odd = contour.flatten()[1::2]  # Y-axis
-    if (even.max() - even.min()) * (odd.max() - odd.min()) < 0.015 * image.shape[0] * image.shape[1]:
+    adaptThreshold = 0.073
+    if 2 * image.shape[0] < image.shape[1]:
+        adaptThreshold = 0.03
+    if (even.max() - even.min()) * (odd.max() - odd.min()) < 0.01 * image.shape[0] * image.shape[1] or \
+            even.max() - even.min() < adaptThreshold * image.shape[1] or \
+            odd.max() - odd.min() < 0.1 * image.shape[0]:
+        return True
+    else:
+        return False
+
+
+def tooThin(contour, image):
+    # type: (ndarray, ndarray) -> bool
+    even = contour.flatten()[0::2]  # X-axis
+    odd = contour.flatten()[1::2]  # Y-axis
+    adaptThreshold = 5
+    if 2 * image.shape[0] < image.shape[1]:
+        adaptThreshold = 20
+    if (even.max() - even.min()) > adaptThreshold * (odd.max() - odd.min()) or \
+            (odd.max() - odd.min()) > adaptThreshold * (even.max() - even.min()):
         return True
     else:
         return False
@@ -72,7 +94,7 @@ def getLabel(file_path=''):
         start = file_path.find('_') + 1
         end = file_path.find('_', start)
         found = file_path[start:end]
-        found = re.sub('[-\.]', '', found)
+        found = re.sub('[-.]', '', found)
     except AttributeError:
         found = ''  # apply your error handling
     return found
@@ -83,8 +105,9 @@ def getChars(img_path='',
              add_contour_dots_to_img=False,
              show_more_info_pics=False,
              draw_contour_rectangle_to_img=False,
+             show_image=False,
              **kwargs):
-    # type: (str, str, bool, bool, bool, ...) -> None
+    # type: (str, str, bool, bool, bool, bool, ...) -> None
 
     if img_path == '':
         logging.ERROR("No image has chosen !!!")
@@ -99,42 +122,52 @@ def getChars(img_path='',
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Brightness balancing
-    equal_histogram = cv2.equalizeHist(gray_img)
+    # equal_histogram = cv2.equalizeHist(gray_img)
 
     # Noise removing
-    noise_removal = cv2.bilateralFilter(equal_histogram, 5, 50, 50)
+    noise_removal = cv2.medianBlur(gray_img, 3)
 
     # Edge Sharpening
-    kernel3 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    thre_mor = cv2.morphologyEx(noise_removal, cv2.MORPH_ERODE, kernel3)
-
-    # Noise filtering
-    # roi_blur = cv2.GaussianBlur(equal_histogram, (3, 3), 1)
+    # kernel3 = np.ones((3, 3), np.uint8)
+    # edge_sharpening = cv2.dilate(noise_removal, kernel3, iterations = 1)
 
     # Convert to Binary Image
-    thre = cv2.adaptiveThreshold(thre_mor, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 41, 0)
+    thre = cv2.adaptiveThreshold(noise_removal, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 41, 0)
 
     _, cont, hier = cv2.findContours(thre,
                                      cv2.RETR_CCOMP,
                                      cv2.CHAIN_APPROX_SIMPLE)
     if show_more_info_pics:
-        cv2.imshow("gray_img", gray_img)
-        cv2.imshow("equal_histogram", equal_histogram)
-        cv2.imshow("noise_removal", noise_removal)
-        cv2.imshow("thre_mor", thre_mor)
-        cv2.imshow("Binary", thre)
-        cv2.drawContours(image, cont, -1, (0, 255, 0), 1)
-        cv2.imshow("Contour", image)
+        thre_gray_img = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                              cv2.THRESH_BINARY_INV, 41, 0)
+        thre_noise_removal = cv2.adaptiveThreshold(noise_removal, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                                   cv2.THRESH_BINARY_INV, 41, 0)
+        # thre_edge_sharpening = cv2.adaptiveThreshold(edge_sharpening, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+        #                                              cv2.THRESH_BINARY_INV, 41, 0)
+        # thre_equal_histogram = cv2.adaptiveThreshold(equal_histogram, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+        #                                              cv2.THRESH_BINARY_INV, 41, 0)
+
+        cv2.imshow("Gray_img", thre_gray_img)
+        # cv2.imshow("Equal_histogram", thre_equal_histogram)
+        cv2.imshow("Noise_removal", thre_noise_removal)
+        # cv2.imshow("Edge_sharpening", thre_edge_sharpening)
+        if add_contour_dots_to_img:
+            cv2.drawContours(image, cont, -1, (0, 255, 0), 1)
+            cv2.imshow("Contour", image)
         cv2.waitKey(0)
 
     areas_idx = {}
     areas = []
     for idx, cnt in enumerate(cont):
+        # cv2.drawContours(image, cnt, -1, (0, 255, 0), 1)
+        # cv2.imshow("Contour", image)
+        # cv2.waitKey(0)
+
         if hier[0][idx][3] == -1 and \
                 not isNearlyMaxLength(cnt, image) and \
-                not isNearlyMinLength(cnt, image):
-            if add_contour_dots_to_img:
-                cv2.drawContours(image, cnt, -1, (0, 255, 0), 3)
+                not isNearlyMinLength(cnt, image) and \
+                not tooThin(cnt, image):
+
             area = cv2.contourArea(cnt)
             if area in areas_idx:
                 area += 1
@@ -150,6 +183,10 @@ def getChars(img_path='',
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
         char_positions.append(np.array([x, y, w, h]))
 
+    if show_image:
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        plt.show()
+
     if char_positions.__len__() == 0:
         logging.info(img_path + " has no contour. Need to check again.")
         return
@@ -159,7 +196,16 @@ def getChars(img_path='',
     char_positions = char_positions[(char_positions[:, 0] + char_positions[:, 1] * 3).argsort()[::1]]
 
     for idx, position in enumerate(char_positions):
-        crop_img = image[position[1]:position[1] + position[3], position[0]:position[0] + position[2]]
+        crop_img = thre[position[1]:position[1] + position[3], position[0]:position[0] + position[2]]
+        max_size = max(crop_img.shape[0], crop_img.shape[1])
+        delta_w = max_size - crop_img.shape[1]
+        delta_h = max_size - crop_img.shape[0]
+        top, bottom = delta_h // 2, delta_h - (delta_h // 2)
+        left, right = delta_w // 2, delta_w - (delta_w // 2)
+        WHITE = [255, 255, 255]
+        BLACK = [0, 0, 0]
+        resized_image = cv2.copyMakeBorder(crop_img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=BLACK)
+        resized_image = cv2.bitwise_not(resized_image)
         # folder_name = getLabel(img_path)
         # if folder_name == '':
         #     return
@@ -171,11 +217,19 @@ def getChars(img_path='',
             if e.errno != errno.EEXIST:
                 raise
         # cv2.imwrite(pth_name + folder_name + '/' + str(idx) + '.jpg', crop_img)
-        cv2.imwrite(img_path[:-4] + '/' + str(idx) + '.jpg', crop_img)
+        cv2.imwrite(img_path[:-4] + '/' + str(idx) + '.jpg', resized_image)
 
 
 if __name__ == '__main__':
     path_name = "./1/"
     for f in list_files(path_name)[:100]:
-        getChars(f, path_name)
-    # getChars("./1/169E620E_34B2-702.34_01022018071608_i3.jpg")
+        getChars(f, path_name,
+                 add_contour_dots_to_img=False,
+                 show_more_info_pics=False,
+                 draw_contour_rectangle_to_img=False,
+                 show_image=False)
+    # getChars("./1/96D62BBE_51D-077.17_01022018092225_i3.jpg",
+    #          add_contour_dots_to_img=False,
+    #          show_more_info_pics=False,
+    #          draw_contour_rectangle_to_img=True,
+    #          show_image=True)
